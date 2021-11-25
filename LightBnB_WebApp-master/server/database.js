b617898c73ebdd79c6486d31f2919ba5f719dbec
jtardioli <joshua.tardioli@gmail.com>
2021-11-25 16:20:15 -0500

    add getReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT *
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  ORDER BY reservations.start_date
+  LIMIT $2;`,
+  
+  [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      if (!result.rows) {
+        return null;
+      }
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
